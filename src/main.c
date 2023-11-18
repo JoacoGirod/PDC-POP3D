da50a235390d560b8fa8be64104056a5a46fe6ba
JoacoGirod <joaquingirod@gmail.com>
2023-11-18 11:14:45 -0300

    Create initial mail management

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -23,18 +25,27 @@
 #include "pop3_parser_automaton.h"
 #include "args.h"
 #include "utils.h"
+#include <dirent.h>
 
-void microTesting()
-{
-    // Used for testing
-}
+#define MAX_PATH_LENGTH 256
+#define MAX_FILENAME_LENGTH 256
+#define MAX_EMAILS 64
 
-static bool done = false;
-static void sigterm_handler(const int signal)
+enum MailStatus
 {
-    printf("\nSignal %d , cleaning up and exiting\n", signal); // make the main logger a global variable?
-    done = true;
-}
+    STATUS_NORMAL,
+    STATUS_DELETED,
+    STATUS_RETRIEVED
+};
+
+struct Mail
+{
+    int index;
+    size_t octets;
+    char filename[MAX_FILENAME_LENGTH];
+    char folder[4]; // Can only be tmp, cur or new
+    enum MailStatus status;
+};
 
 /**
  * Structure used to transport information from the distributor thread onto the client handler threads
@@ -45,7 +56,123 @@ struct connection
     socklen_t addrlen;
     struct sockaddr_in6 addr;
     Logger *logger;
+    struct Mail mails[MAX_EMAILS];
+    size_t numEmails;
 };
+
+void retrieve_emails(const char *user_path, struct connection *conn);
+void retrieve_emails_from_directory(const char *user_path, const char *dir_name, struct connection *conn);
+int get_next_index();
+size_t get_file_size(const char *dir_path, const char *file_name);
+
+void retrieve_emails(const char *user_path, struct connection *conn)
+{
+    // Process emails in 'cur' directory
+    retrieve_emails_from_directory(user_path, "cur", conn);
+
+    // Process emails in 'new' directory
+    retrieve_emails_from_directory(user_path, "new", conn);
+}
+
+void retrieve_emails_from_directory(const char *user_path, const char *dir_name, struct connection *conn)
+{
+    char dir_path[MAX_PATH_LENGTH];
+    snprintf(dir_path, MAX_PATH_LENGTH, "%s/%s", user_path, dir_name);
+    printf("Path %s\n", dir_path);
+    DIR *dir = opendir(dir_path);
+    if (dir == NULL)
+    {
+        perror("Error opening directory");
+        return;
+    }
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL)
+    {
+        if (entry->d_type == DT_REG)
+        { // Check if it's a regular file
+            if (conn->numEmails < MAX_EMAILS)
+            {
+                struct Mail mail;
+                mail.index = get_next_index();
+                mail.octets = get_file_size(dir_path, entry->d_name);
+                snprintf(mail.filename, MAX_FILENAME_LENGTH, "%s", entry->d_name);
+                snprintf(mail.folder, 4, "%s", dir_name);
+                mail.status = STATUS_NORMAL;
+
+                // Add the mail to the user's mailbox
+                conn->mails[conn->numEmails++] = mail;
+
+                // Process the mail or store it as needed
+                // (You can add your logic here)
+
+                // Print information about the mail
+                printf("Mail Index: %d\n", mail.index);
+                printf("Mail Octets: %lu\n", mail.octets);
+                printf("Mail Filename: %s\n", mail.filename);
+                printf("Mail Folder: %s\n", mail.folder);
+                printf("Mail Status: %d\n", mail.status);
+            }
+            else
+            {
+                // Handle the case when the mailbox is full
+                fprintf(stderr, "Mailbox is full for user\n");
+                break;
+            }
+        }
+    }
+
+    closedir(dir);
+}
+
+int get_next_index()
+{
+    static int index = 0;
+    return ++index;
+}
+
+size_t get_file_size(const char *dir_path, const char *file_name)
+{
+    char file_path[MAX_PATH_LENGTH];
+    snprintf(file_path, MAX_PATH_LENGTH, "%s/%s", dir_path, file_name);
+
+    FILE *file = fopen(file_path, "rb");
+    if (file == NULL)
+    {
+        perror("Error opening file");
+        return 0;
+    }
+
+    fseek(file, 0, SEEK_END);
+    size_t size = ftell(file);
+    fclose(file);
+
+    return size;
+}
+
+// -----------------------------------------------------------------------------------------------------------
+
+void microTesting()
+{
+    char *user_path = "/tmp/Maildir/testuser";
+
+    // Initialize a connection for the user
+    struct connection conn;
+    conn.numEmails = 0; // Initialize the number of emails to 0
+
+    // Process emails in 'cur' directory for the user
+    retrieve_emails_from_directory(user_path, "cur", &conn);
+
+    // Process emails in 'new' directory for the user
+    retrieve_emails_from_directory(user_path, "new", &conn);
+}
+
+static bool done = false;
+static void sigterm_handler(const int signal)
+{
+    printf("\nSignal %d , cleaning up and exiting\n", signal); // make the main logger a global variable?
+    done = true;
+}
 
 /**
  * Main Handling Function for incoming requests
---
+++ src/populator.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# Function to create Maildir structure and populate with sample emails
+create_maildir_structure() {
+    local maildir_path="/tmp/Maildir"
+    mkdir -p "$maildir_path"
+
+    local user_path="$maildir_path/testuser"
+    mkdir -p "$user_path"
+
+    for folder in "new" "tmp" "cur"; do
+        mkdir -p "$user_path/$folder"
+    done
+
+    # Sample emails
+    local email_content1="Return-Path: <joaquin@foo.pdc>\n
+                            X-Original-To: joaquin@foo.pdc\n
+                            Delivered-To: joaquin@foo.pdc\n
+                            Received: from jgirod (localhost [127.0.0.1])\n
+                                by jgirod.foo.pdc (Postfix) with ESMTP id 1B6D917F014\n
+                                for <joaquin@foo.pdc>; Thu, 31 Aug 2023 11:19:47 -0300 (-03)\n
+                            subject: felicidad\n
+                            Message-Id: <20230831141950.1B6D917F014@jgirod.foo.pdc>\n
+                            Date: Thu, 31 Aug 2023 11:19:47 -0300 (-03)\n
+                            From: joaquin@foo.pdc\n\n
+                            from jose@gmail.com\n
+                            dfsasd\n"
+    local email_content2="Return-Path: <joaquin@foo.pdc>\n
+                        X-Original-To: joaquin@foo.pdc\n
+                        Delivered-To: joaquin@foo.pdc\n
+                        Received: from jgirod (unknown [192.168.1.6])\n
+                            by joaquin.foo.pdc (Postfix) with ESMTP id 4075017ED0A\n
+                            for <joaquin@foo.pdc>; Thu, 31 Aug 2023 11:33:33 -0300 (-03)\n
+                        Subject: What is love \n
+                        From: <flopezmenardi@itba.edu.ar>\n
+                        To: <cijjas@itba.edu.ar>\n
+                        Message-Id: <950124.162336@example.com>\n
+                        Date: Thu, 31 Aug 2023 11:33:33 -0300 (-03)\n
+
+                        This is a great message
+                        "
+    local email_content3="A : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        B : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        C : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        D : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        E : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        F : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        G : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        H : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        I : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        J : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        K : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        L : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        M : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        N : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        O : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        P : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        Q : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        R : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        S : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        T : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        U : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        V : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        W : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        Y : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        Z : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n
+                        "
+    # Populate cur directory with sample emails
+    echo -e "$email_content1" >"$user_path/cur/email1"
+    echo -e "$email_content2" >"$user_path/cur/email2"
+
+    # Populate new directory with sample emails
+    echo -e "$email_content3" >"$user_path/new/email3"
+    echo -e "$email_content2" >"$user_path/new/email4"
+
+    echo "Maildir structure created successfully."
+}
+
+# Create Maildir structure and populate with sample emails
+create_maildir_structure
+
+# Create a 100MB file using dd command
+dd if=/dev/urandom of=/tmp/Maildir/testuser/cur/large_email bs=4096 count=25600
+
+echo "100MB file created successfully."
